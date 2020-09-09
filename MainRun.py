77b1512f33a6d9e7a8c2364e474a810543e3c7a8
“Aye10032 <yeyu6_505@126.com>
2020-09-09 11:13:07 +0800

    更新依赖

--- MainRun.py
+++ MainRun.py
@@ -26,7 +26,7 @@ else:
     # we are running in a normal Python environment
     basedir = os.path.dirname(__file__)
 
-VERSION = 'V4.0.1'
+VERSION = 'V4.1.0'
 RES_PATH = 'res'
 LOG_PATH = 'log'
 CONFIG_PATH = 'res/config.json'
