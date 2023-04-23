619586d6b5352c2e0a6a38c764c9805a81e5625c
Aye10032 <aye10032@gmail.com>
2023-04-23 13:56:09 +0800

    语言文件

---
+++ res/lang/zh_CN.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="Main.py" line="55"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="Main.py" line="58"/>
+        <source>Local Video</source>
+        <translation>历史记录</translation>
+    </message>
+    <message>
+        <location filename="Main.py" line="61"/>
+        <source>Subscription Information</source>
+        <translation>订阅信息</translation>
+    </message>
+    <message>
+        <location filename="Main.py" line="64"/>
+        <source>TODO List</source>
+        <translation>待翻译列表</translation>
+    </message>
+    <message>
+        <location filename="Main.py" line="70"/>
+        <source>Info</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="Main.py" line="74"/>
+        <source>Setting</source>
+        <translation>设置</translation>
+    </message>
+</context>
+</TS>
