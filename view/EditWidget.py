2699acfb2e5353d9fff31a35816647f1b4c2c8d1
Aye10032 <aye10032@gmail.com>
2023-04-27 23:35:18 +0800

    主界面布局优化

--- view/EditWidget.py
+++ view/EditWidget.py
@@ -1,6 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QFrame, QSplitter, QGridLayout, QLabel, QPushButton, QListWidget, QWidget, QPlainTextEdit, \
-    QVBoxLayout, QSizePolicy, QLineEdit, QHBoxLayout
+from PyQt5.QtWidgets import QFrame, QGridLayout, QLabel, QWidget, QSizePolicy, QHBoxLayout
 from qfluentwidgets import LineEdit, PushButton, ToolButton, SwitchButton, TextEdit
 from qfluentwidgets import FluentIcon as FIF
 
@@ -50,6 +49,7 @@ class EditWidget(QFrame):
 
     def init_ui(self):
         self.main_layout.setSpacing(0)
+        self.main_layout.setContentsMargins(20, 5, 20, 5)
         for i in range(9):
             self.main_layout.setColumnStretch(i, 1)
             self.main_layout.setRowStretch(i, 0)
@@ -57,19 +57,22 @@ class EditWidget(QFrame):
         self.main_layout.setRowStretch(7, 1)
         self.main_layout.setRowStretch(8, 1)
 
+        self.title_label.setMargin(10)
         self.main_layout.addWidget(self.title_label, 0, 0, 1, 9, Qt.AlignCenter)
 
         widget_1 = QWidget()
         layout_1 = QHBoxLayout()
+        layout_1.setContentsMargins(0, 5, 0, 5)
         self.origin_link_input.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         layout_1.addWidget(self.origin_link_label, stretch=1)
         layout_1.addWidget(self.origin_link_input, stretch=6)
-        # print(self.origin_link_label.height(), self.origin_link_input.height())
         widget_1.setLayout(layout_1)
         self.main_layout.addWidget(widget_1, 1, 0, 1, 9)
 
         widget_2 = QWidget()
         layout_2 = QGridLayout()
+        # layout_2.setSpacing(0)
+        layout_2.setContentsMargins(0, 0, 0, 5)
         self.quality_input.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         layout_2.addWidget(self.auto_quality_label, 0, 0, Qt.AlignLeft)
         layout_2.addWidget(self.auto_quality_btn, 0, 1, Qt.AlignLeft)
@@ -84,6 +87,7 @@ class EditWidget(QFrame):
 
         widget_3 = QWidget()
         layout_3 = QHBoxLayout()
+        layout_3.setContentsMargins(0, 15, 0, 5)
         layout_3.addWidget(self.video_title_label, stretch=1)
         layout_3.addWidget(self.video_title_input, stretch=6)
         layout_3.addWidget(self.copy_title_btn, stretch=1)
@@ -93,6 +97,7 @@ class EditWidget(QFrame):
 
         widget_4 = QWidget()
         layout_4 = QHBoxLayout()
+        layout_4.setContentsMargins(0, 0, 0, 5)
         layout_4.addWidget(self.reprint_info_label, stretch=1)
         layout_4.addWidget(self.reprint_info_input, stretch=6)
         layout_4.addWidget(self.copy_reprint_btn, stretch=1)
@@ -106,20 +111,21 @@ class EditWidget(QFrame):
         self.video_description_input.setText(
             '作者：\r\n发布时间：\r\n搬运：\r\n视频摘要：\r\n原简介翻译：\r\n存档：\r\n其他外链：')
 
-        self.main_layout.addWidget(self.play_btn, 9, 1, Qt.AlignHCenter)
-        self.main_layout.addWidget(self.copy_btn, 9, 3, Qt.AlignHCenter)
-        self.main_layout.addWidget(self.link_btn, 9, 5, Qt.AlignHCenter)
-        self.main_layout.addWidget(self.folder_btn, 9, 7, Qt.AlignHCenter)
+        widget_5 = QWidget()
+        layout_5 = QHBoxLayout()
+        layout_5.setContentsMargins(0, 5, 0, 15)
+        layout_5.addWidget(self.play_btn, stretch=1)
+        layout_5.addWidget(self.copy_btn, stretch=1)
+        layout_5.addWidget(self.link_btn, stretch=1)
+        layout_5.addWidget(self.folder_btn, stretch=1)
+        widget_5.setLayout(layout_5)
+        self.main_layout.addWidget(widget_5, 9, 0, 1, 9)
 
         self.log_output.setFixedHeight(100)
         self.log_output.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         self.main_layout.addWidget(self.log_output, 10, 0, 2, 9)
-
-        # for i in range(self.main_layout.rowCount()):
-        #     for j in range(self.main_layout.columnCount()):
-        #         # self.main_layout.addWidget(QLabel(f"{i},{j}"), i, j)
-        #         try:
-        #             self.main_layout.itemAtPosition(i, j).widget().setStyleSheet("border: 1px solid black;")
+        self.log_output.setStyleSheet('font-size: 12px;font-family: \'Segoe UI\', \'Microsoft YaHei\';')
+        self.log_output.setReadOnly(True)
 
         self.setLayout(self.main_layout)
 
